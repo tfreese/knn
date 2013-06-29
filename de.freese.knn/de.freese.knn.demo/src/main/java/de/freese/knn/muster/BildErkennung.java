1ae927b4613b376c112278d8b9bd6bc0b53cdee2
tfreese <commercial@freese-home.de>
2013-06-29 09:28:41 +0200

    bilderkennung aktualisiert

--- de.freese.knn/de.freese.knn.demo/src/main/java/de/freese/knn/muster/BildErkennung.java
+++ de.freese.knn/de.freese.knn.demo/src/main/java/de/freese/knn/muster/BildErkennung.java
@@ -7,7 +7,7 @@ import de.freese.knn.net.NeuralNet;
 import de.freese.knn.net.layer.hidden.SigmoidLayer;
 import de.freese.knn.net.layer.input.InputLayer;
 import de.freese.knn.net.layer.output.OutputLayer;
-import de.freese.knn.net.math.executor.ExecutorKnnMath;
+import de.freese.knn.net.math.forkjoin.ForkJoinKnnMath;
 import de.freese.knn.net.trainer.ITrainingInputSource;
 import de.freese.knn.net.trainer.NetTrainer;
 import de.freese.knn.net.trainer.PrintStreamNetTrainerListener;
@@ -20,86 +20,86 @@ import java.io.OutputStream;
 /**
  * Testklasse.
  * <p/>
+ * 
  * @author Thomas Freese
  */
 public class BildErkennung
 {
-    /**
-     * @param args String[]
-     * <p/>
-     * @throws Exception Falls was schief geht.
-     */
-    public static void main(final String[] args) throws Exception
-    {
-        NeuralNet neuralNet = null;
-        // ITrainingInputSource trainingInputSource = new ImagePixelTrainingInputSource();
-        ITrainingInputSource trainingInputSource = new ImageInfoTrainingInputSource();
-        File nnFile = new File("ImageNeuralNet.bin");
-        // if (!nnFile.exists())
-        // if (true)
-        {
-            // neuralNet = new NeuralNet();
-            neuralNet = new NeuralNet(new ExecutorKnnMath());
-//			neuralNet = new NeuralNet(new ForkJoinKnnMath());
-            neuralNet.addLayer(new InputLayer(36));
-            neuralNet.addLayer(new SigmoidLayer(50));
-            neuralNet.addLayer(new OutputLayer(10));
-            neuralNet.connectLayer();
+	/**
+	 * @param args String[]
+	 *            <p/>
+	 * @throws Exception Falls was schief geht.
+	 */
+	public static void main(final String[] args) throws Exception
+	{
+		NeuralNet neuralNet = null;
+		// ITrainingInputSource trainingInputSource = new ImagePixelTrainingInputSource();
+		ITrainingInputSource trainingInputSource = new ImageInfoTrainingInputSource();
+		File nnFile = new File("ImageNeuralNet.bin");
+		// if (!nnFile.exists())
+		// if (true)
+		{
+			// neuralNet = new NeuralNet();
+			// neuralNet = new NeuralNet(new ExecutorKnnMath());
+			neuralNet = new NeuralNet(new ForkJoinKnnMath());
+			neuralNet.addLayer(new InputLayer(36));
+			neuralNet.addLayer(new SigmoidLayer(50));
+			neuralNet.addLayer(new OutputLayer(10));
+			neuralNet.connectLayer();
 
-            double teachFactor = 0.5D;
-            double momentum = 0.5D;
-            double maximumError = 0.01D;
-            int maximumIteration = Integer.MAX_VALUE;
+			double teachFactor = 0.5D;
+			double momentum = 0.5D;
+			double maximumError = 0.01D;
+			int maximumIteration = Integer.MAX_VALUE;
 
-            long start = System.currentTimeMillis();
+			long start = System.currentTimeMillis();
 
-            NetTrainer trainer =
-                    new NetTrainer(teachFactor, momentum, maximumError, maximumIteration);
-            trainer.addNetTrainerListener(new PrintStreamNetTrainerListener(System.out, 500));
-            // trainer.addNetTrainerListener(new LoggerNetTrainerListener(500));
-            trainer.train(neuralNet, trainingInputSource);
+			NetTrainer trainer = new NetTrainer(teachFactor, momentum, maximumError, maximumIteration);
+			trainer.addNetTrainerListener(new PrintStreamNetTrainerListener(System.out, 500));
+			// trainer.addNetTrainerListener(new LoggerNetTrainerListener(500));
+			trainer.train(neuralNet, trainingInputSource);
 
-            System.out.println("Lernzeit: " + ((System.currentTimeMillis() - start) / 1000) + " s");
+			System.out.println("Lernzeit: " + ((System.currentTimeMillis() - start) / 1000) + " s");
 
-            Toolkit.getDefaultToolkit().beep();
+			Toolkit.getDefaultToolkit().beep();
 
-            // Speichern
-            try (OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(nnFile)))
-            {
-                neuralNet.save(outputStream);
-            }
-        }
-        // else
-        // {
-        // InputStream inputStream = new BufferedInputStream(new FileInputStream(nnFile));
-        // neuralNet.load(inputStream);
-        // inputStream.close();
-        // }
+			// Speichern
+			try (OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(nnFile)))
+			{
+				neuralNet.save(outputStream);
+			}
+		}
+		// else
+		// {
+		// InputStream inputStream = new BufferedInputStream(new FileInputStream(nnFile));
+		// neuralNet.load(inputStream);
+		// inputStream.close();
+		// }
 
-        // Netz testen
-        for (int i = 0; i < trainingInputSource.getSize(); i++)
-        {
-            double[] inputs = trainingInputSource.getInputAt(i);
-            double[] outputs = neuralNet.getOutput(inputs);
+		// Netz testen
+		for (int i = 0; i < trainingInputSource.getSize(); i++)
+		{
+			double[] inputs = trainingInputSource.getInputAt(i);
+			double[] outputs = neuralNet.getOutput(inputs);
 
-            for (double output : outputs)
-            {
-                // System.out.print(output + ", ");
-                System.out.print(String.format("   %1.10f", Double.valueOf(output)));
-            }
+			for (double output : outputs)
+			{
+				// System.out.print(output + ", ");
+				System.out.print(String.format("   %1.10f", Double.valueOf(output)));
+			}
 
-            System.out.println();
-        }
+			System.out.println();
+		}
 
-        try
-        {
-            neuralNet.release();
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }
+		try
+		{
+			neuralNet.release();
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
 
-        System.exit(0);
-    }
+		System.exit(0);
+	}
 }
